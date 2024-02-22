8c2639bdd095e7970b75ce01f85b4bcd93befb14
albrthuynh <128447235+albrthuynh@users.noreply.github.com>
2024-02-22 00:33:39 -0600

    Add files via upload

---
+++ 342Prog1-main-2/Project1/src/main/java/BinarySearchTreeDict.java
@@ -0,0 +1,208 @@
+import java.util.Iterator;
+
+public class BinarySearchTreeDict<K extends Comparable<K>,V> implements ProjOneDictionary<K,V> {
+    private int size = 0;
+    private boolean isDuplicate = false;
+    private Node root = null;
+
+    private class Node {
+        K key;
+        V value;
+        Node left;
+        Node right;
+        Node parent; // is this needed???
+        boolean isDuplicate = false;
+
+        Node (K newKey, V newValue) {
+            super();
+            key = newKey;
+            value = newValue;
+            left = null;
+            right = null;
+            parent = null;
+        }
+    }
+
+    private boolean recursiveInsertion (Node current, K key, V value) {
+        if (key.compareTo(current.key) < 0) {
+            // go left
+            if (current.left != null) {
+                // keep going into a null spot is found
+                return recursiveInsertion(current.left, key, value);
+            }
+            else {
+                current.left = new Node(key, value);
+                current.left.parent = current;
+                size++;
+                return false;
+            }
+        }
+        else if (key.compareTo(current.key) > 0) {
+            // got right
+            if (current.right != null) {
+                return recursiveInsertion(current.right, key, value);
+            }
+            else {
+                current.right = new Node(key, value);
+                current.right.parent = current;
+                size++;
+                return false;
+            }
+        }
+        else {
+            // key already exists
+            current.value = value;
+            return true;
+        }
+    }
+
+    private Node recursiveSearch (Node current, K key) {
+        // if root is null then key wasn't found
+        if (current == null) {
+            return current;
+        }
+        else if (key.compareTo(current.key) == 0) {
+            return current;
+        }
+        else if (key.compareTo(current.key) < 0) {
+            return recursiveSearch(current.left, key);
+        }
+        else {
+             return recursiveSearch(current.right, key);
+        }
+    }
+
+    private Node successor(Node root) {
+        root = root.right;
+        while (root.left != null) {
+            root = root.left;
+        }
+        
+        return root;
+    }
+    
+    private Node predecessor(Node root){
+        root = root.left;
+        while (root.right != null){
+            root = root.right;
+        }
+        return root;
+    }
+    // recursiveDelete (recursively deletes a node)
+    // returns false when it doesn't find the node to delete
+    // returns true when it does find the node to delete
+    private boolean recursiveDelete(Node curr, K key){
+
+        // key not found
+        if (curr == null) {
+            System.out.println("KEY WASNT FOUND");
+            return false;
+        }
+
+        // finding key
+        if (key.compareTo(curr.key) > 0) {
+            System.out.println("KEY IS GREATER THAN CURR SO GO RIGHT");
+            return recursiveDelete(curr.right, key);
+        }
+        else if (key.compareTo(curr.key) < 0) {
+            System.out.println("KEY IS LESS THAN CURR SO GO LEFT");
+            return recursiveDelete(curr.left, key);
+        }
+        // key found!!!
+        else{
+            System.out.println("Key found!");
+
+            // delete if leaf node
+            if (curr.right == null && curr.left == null){
+                System.out.println("deleting leaf node!");
+
+
+                if (curr.parent != null) {
+                    //If the parent right is the same as the current node then delete
+                    if (curr.parent.right == curr) {
+                        curr.parent.right = null;
+                    } else if (curr.parent.left == curr) {
+                        curr.parent.left = null;
+                    }
+                }
+                else {
+                    root = null;
+                }
+
+                size--;
+                return true;
+            }
+            // if curr has a right child
+            else if (curr.right != null){
+                System.out.println("Key has right child!");
+                curr.key = successor(curr).key;
+                curr.value = successor(curr).value;
+                return recursiveDelete(curr.right, curr.key);
+            }
+            // if curr has left child
+            else{
+                System.out.println("Key has left Child");
+                curr.key = predecessor(curr).key;
+                curr.value = predecessor(curr).value;
+                return recursiveDelete(curr.left, curr.key);
+            }
+        }
+    }
+
+    @Override
+    public boolean insert(K key, V value) throws NullValueException {
+
+        if (value == null) {
+            throw new NullValueException();
+        }
+
+        if (root == null) {
+            root = new Node(key, value);
+            root.parent = null;
+            size++;
+            return false;
+        }
+        else {
+            return recursiveInsertion(root, key, value);
+        }
+
+    }
+
+    @Override
+    public V find(K key) {
+        Node foundValue = recursiveSearch(root, key);
+        
+        if (foundValue != null) {
+            return foundValue.value;
+        }
+
+        return null;
+    }
+
+    @Override
+    public boolean delete(K key) {
+        if(root == null){
+            System.out.println("root null");
+            return false;
+        }
+        boolean isDeleted = recursiveDelete(root, key);
+
+
+        if (isDeleted) {
+            return true;
+        }
+
+        return false;
+    }
+
+
+    @Override
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    public Iterator<K> iterator() {
+        return null;
+    }
+}
---
+++ 342Prog1-main-2/Project1/src/test/java/BSTTest.java
@@ -0,0 +1,91 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class BSTTest extends DictionaryTest {
+    @Override
+    public ProjOneDictionary<Integer, String> newDictionary() {
+        return new BinarySearchTreeDict<Integer,String>();
+    }
+
+    @Test
+    void singleElementInsert() throws NullValueException {
+        ProjOneDictionary<Integer, String> BST = newDictionary();
+        assertFalse(BST.insert(3, "three"), "Incorrect single insert return statement");
+        assertEquals("three", BST.find(3), "Incorrect single element insert behavior");
+    }
+
+    @Test
+    void multipleElementInsert() throws NullValueException {
+        ProjOneDictionary<Integer, String> BST = newDictionary();
+        assertFalse(BST.insert(6, "six"), "Incorrect multiple element insert return statement");
+        assertFalse(BST.insert(4, "four"), "Incorrect multiple element insert return statement");
+        assertFalse(BST.insert(7, "seven"), "Incorrect multiple element insert return statement");
+
+        assertEquals("six", BST.find(6), "Incorrect multiple element insert behavior");
+        assertEquals("four", BST.find(4), "Incorrect multiple element insert behavior");
+        assertEquals("seven", BST.find(7), "Incorrect multiple element insert behavior");
+    }
+
+    @Test
+    void multipleElementInsertWithDuplicate() throws NullValueException {
+        ProjOneDictionary<Integer, String> BST = newDictionary();
+        assertFalse(BST.insert(6, "six"), "Incorrect multiple element insert return statement");
+        assertFalse(BST.insert(4, "four"), "Incorrect multiple element insert return statement");
+        assertTrue(BST.insert(6, "seven"), "Incorrect multiple element insert return statement");
+
+        assertEquals("seven", BST.find(6), "Incorrect multiple element insert behavior");
+        assertEquals("four", BST.find(4), "Incorrect multiple element insert behavior");
+        assertNotEquals("six", BST.find(6), "Incorrect multiple element insert behavior");
+        assertNotEquals(null, BST.find(6), "Incorrect multiple element insert behavior");
+        assertEquals(null, BST.find(5), "Incorrect multiple element insert behavior");
+    }
+
+    @Test
+    void emptyGetSize() throws NullValueException {
+        ProjOneDictionary<Integer, String> BST = newDictionary();
+
+        assertEquals(0, BST.getSize(), "Incorrect empty getSize behavior");
+    }
+
+    @Test
+    void singleElementGetSize () throws NullValueException {
+        ProjOneDictionary<Integer, String> BST = newDictionary();
+        BST.insert(6, "six");
+
+        assertEquals(1, BST.getSize(), "Incorrect empty getSize behavior");
+    }
+
+    @Test
+    void multipleElementGetSize () throws NullValueException {
+        ProjOneDictionary<Integer, String> BST = newDictionary();
+        BST.insert(6, "six");
+        BST.insert(4, "four");
+        BST.insert(7, "four");
+
+        assertEquals(3, BST.getSize(), "Incorrect empty getSize behavior");
+    }
+
+//    @Test
+//    void singleElementDelete () throws NullValueException {
+//        ProjOneDictionary<Integer, String> BST = newDictionary();
+//        BST.insert(6, "six");
+//
+//        assertTrue(BST.delete(6), "Incorrect single element delete behavior");
+//        assertFalse(BST.delete(8), "Incorrect single element delete behavior");
+//        assertEquals(0, BST.getSize(), "Incorrect single element delete behavior");
+//    }
+
+
+    @Test
+    void multipleElementDelete () throws NullValueException {
+        ProjOneDictionary<Integer, String> BST = newDictionary();
+        BST.insert(6, "six");
+        BST.insert(4, "four");
+        BST.insert(7, "seven");
+
+        assertTrue(BST.delete(6), "Incorrect multiple element delete");
+        assertTrue(BST.delete(4), "Incorrect multiple element delete");
+        assertTrue(BST.delete(7), "Incorrect multiple element delete");
+//        assertFalse(BST.delete(4), "Incorrect multiple element delete");
+    }
+}
