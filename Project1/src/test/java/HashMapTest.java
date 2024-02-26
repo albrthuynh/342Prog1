35ac6dc9a70b5fc9b778452748532ec219dcfff0
Karina <97719411+karinaa0405@users.noreply.github.com>
2024-02-25 22:56:08 -0600

    Add files via upload

--- Project1/src/test/java/HashMapTest.java
+++ Project1/src/test/java/HashMapTest.java
@@ -1,6 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 public class HashMapTest extends DictionaryTest{
     @Override
     public ProjOneDictionary<Integer, String> newDictionary() {
         return new HashMapDict<Integer,String>();
+    }
+
+    @Test
+    void singleElementInsert() throws NullValueException {
+        ProjOneDictionary<Integer, String> HASHMAP = newDictionary();
+        assertFalse(HASHMAP.insert(3, "three"), "Incorrect single insert return statement");
+        assertEquals("three", HASHMAP.find(3), "Incorrect single element insert behavior");
     }
 }
